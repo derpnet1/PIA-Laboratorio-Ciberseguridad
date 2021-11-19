eda2776e50aae099d8c5a16803d499a5522b3912
derpnet1 <89653100+derpnet1@users.noreply.github.com>
2021-11-19 09:44:06 -0600

    T2-PowerShell
    Este script esta creado en PowerShell y esta dividido en 1 archivo.ps1 y 1 modulo.psm1 y tiene la funcionalidad de escanear la subred, los puertos para un equipo y los puertos parar todos los equipos activos en la red y esto por medio de un menu que contiene las 3 opciones.

---
+++ T2-Pshell_1967673.ps1
@@ -0,0 +1,52 @@
+﻿#Laboratorio de ciberseguridad.
+#Tarea 2 powershell .
+#Escaneo de subred, puertos del equipo y puertos activos en la red.
+
+#Elaborado por: Daniel Ernesto Rangel Perez .
+#Matricual: 1967673 
+#Fecha: 15/10/2021
+
+
+#Importamos nuestro modulo en la direccion de modulos de powershell pero en un carpeta creada llamada Tarea.
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\Tarea\T2-powershell_1967673.psm1"
+
+#Con la variable $a se abre un ciclo repetitivo para mostrar y solicitar nuestra opcion a ejecutar. 
+$a = 0
+while($a -eq 0){
+ $opc = Read-Host "
+        
+        Menu:
+ [1]-Escaneo de Subred
+ [2]-Escaneo de puertos para un equipo
+ [3]-Escaneo de puertos para todos los equipos que estén activos en la red
+ [0]-Salir
+
+ Elige una opcion"
+ 
+  switch($opc){
+    1{
+        write-host ""
+        ScanSubred
+        Write-Host "*********************************************"
+        break
+    }
+    2{
+        write-host ""
+        PuertosEquipo
+        Write-Host "*********************************************"
+        break
+    }
+    3{
+        write-host ""
+        PuertosRedx
+        Write-Host "*********************************************"
+        break
+    }
+    0{
+        $a = 1
+        Write-Host ""
+        Write-Host "Adios :)"
+        Write-Host "Produced by: Daniel Ernesto Rangel Perez"
+    }
+  } 
+}
---
+++ T2-powershell_1967673.psm1
@@ -0,0 +1,117 @@
+﻿function ScanSubred {
+
+    #Determinando gateway
+    Write-Host "       Escaneo Subred"
+    $subred = (Get-NetRoute -DestinationPrefix 0.0.0.0/0).NextHop
+    Write-Host "Tu gateway: $subred"
+
+    #Determinando el rango de la subred
+    $rango = $subred.Substring(0,$subred.IndexOf('.') + 1 + $subred.Substring($subred.IndexOf('.') + 1).IndexOf('.') + 3)
+    echo $rango
+
+    Write-Host "***Este proceso puede tardar unos minutos ...***"
+
+    #Se forza a terminar en punto el rango de la subred
+    $punto = $rango.EndsWith('.')
+        if ( $punto -like "False" )
+        {
+            $rango = $rango +  '.' 
+        }
+        
+    # Creamos un array con 254 numeros En la variable rango_ip   
+    $rango_ip = @(1..254)
+
+    #Validamos los host activos con un ciclo.
+    foreach ( $r in $rango_ip )
+        {
+            $actual = $rango + $r 
+            $responde = Test-Connection $actual -Quiet -Count 1 
+            if ( $responde -eq "True" )
+            {
+                Write-Output ""
+                Write-Host "Host activo: " -NoNewline; Write-Host $actual -ForegroundColor Green
+            }
+        }
+
+}
+
+
+function PuertosEquipo {
+    
+    #Determinando gateway 
+    write-host "Puertos activos en el equipo"
+    $subred = (Get-NetRoute -DestinationPrefix 0.0.0.0/0).NextHop
+    Write-Host "== Determinado tu gateway ..."
+    Write-Host "Tu gateway es: $subred"
+
+    #Determinando el rango de la subred
+    $rango = $subred.Substring(0,$subred.IndexOf(".") + 1 + $subred.Substring($subred.IndexOf(".") +1).IndexOf(".") +3)
+    write-host "== Determinado tu rango de subred ..."
+    echo $rango
+
+    #Se forza a terminar en punto el rango de la subred
+    $punto = $rango.endswith(".")
+    if ($punto -like "false"){
+        $rango = $rango + "."
+    }
+
+    #Definimos un array con los puertos a escanear y se establece una variable waittime
+    $portstoscan = @(20,21,22,23,25,50,53,80,110,119,135,136,137,138,143,161,162,389,443,636,1025,1443,3389,5985,5986,8080,10000)
+    $waittime = 100
+
+    #Se solicita la direccion IP a escanear
+    Write-Host "Direccion ip a escanear: " -NoNewline
+    $direccion = Read-Host
+    Write-Host "***Este proceso puede tardar ...***"
+
+    #Evaluamos cada puerto del array $portstoscan con un ciclo.
+    foreach ($p in $portstoscan){
+        $TCPObject = New-Object System.Net.Sockets.TcpClient
+            try{$resultado = $TCPObject.ConnectAsync($direccion,$p).Wait($waittime)}catch{}
+            if ($resultado -eq "True"){
+                Write-Host "Puerto abierto: " -NoNewline; Write-Host $p -ForegroundColor Green
+            }
+    }
+}
+
+
+function PuertosRedx {
+   
+    Write-Host "Puertos activos en la red"
+    #Determinando gateway
+    $subred = (Get-NetRoute -DestinationPrefix 0.0.0.0/0).NextHop
+    Write-Host "==Determinando tu gateway..."
+    Write-Host "Tu gateway: $subred"
+
+    #Determinando el rango de la subred
+    $rango = $subred.Substring(0,$subred.IndexOf('.') + 1 + $subred.Substring($subred.IndexOf('.') + 1).IndexOf('.') + 3)
+    Write-Host "Determinando tu rango de subred..."
+    echo $rango
+    
+    Write-Host "***Este proceso puede tardar unos minutos ...***"
+    
+    #Se forza a terminar en punto el rango de la subred
+    $punto = $rango.EndsWith('.')
+    if ( $punto -like "False" )
+    {
+        $rango = $rango +  '.' 
+    }
+ 
+    $rango_ip = @(1..254)
+    
+    #Validamos los puertos activos en la red con el ciclo foreach.
+    Write-Output ""
+    Write-Host "-- Subred actual:"
+    Write-Host "Escaneando: " -NoNewline ; Write-Host $rango -NoNewline; Write-Host "0/24" -ForegroundColor Red
+    Write-Output ""
+    foreach ( $r in $rango_ip )
+    {
+        $actual = $rango + $r 
+        $responde = Test-Connection $actual -Quiet -Count 1 
+        if ( $responde -eq "True" )
+        {
+            Write-Output ""
+            Write-Host "Puerto responde: " -NoNewline; Write-Host $actual -ForegroundColor Green
+        }
+    }
+}
